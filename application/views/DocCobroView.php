369b98fa0ea8e32ab463a45c54f722a33640dd73
Kevin <ruben9261@gmail.com>
2018-01-06 21:20:44 -0500

    cobro

--- application/views/DocCobroView.php
+++ application/views/DocCobroView.php
@@ -82,6 +82,39 @@
         </form>
     </div>
 
+
+<div class="modal fade" id="ClientesModal" data-backdrop="static" data-keyboard="false" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 style="color:red;"> Buscar Cliente</h4>
+        </div>
+        <div class="modal-body">
+		<table id="TabalClientes" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Cliente</th>
+                <th>Documento</th>
+                <th>Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-default btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+          
+        </div>
+      </div>
+    </div>
+  </div> 
+
     <script src="/public/libs/jquery/jquery-3.2.1.min.js"></script>
     <script src="/public/libs/bootstrap/bootstrap.min.js"></script>
 </body>
